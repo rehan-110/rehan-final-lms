614844394c67b5e50b739689a84d7d7477aa56d8
Ahmed Rehan <ar4084480@gmail.com>
2025-08-09 09:26:35 +0500

    rehan-final-lms

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,15 +3,16 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCRnVeF1wMWOR4Ndk2DjXK7G5bxN5m2xI0",
-  authDomain: "lms-fp.firebaseapp.com",
-  databaseURL: "https://lms-fp-default-rtdb.firebaseio.com",
-  projectId: "lms-fp",
-  storageBucket: "lms-fp.firebasestorage.app",
-  messagingSenderId: "281763520622",
-  appId: "1:281763520622:web:37b14127f6f1cb9084867f"
+  apiKey: "********************",
+  authDomain: "(((((((((((((((((((((",
+  databaseURL: "******************",
+  projectId: "*******",
+  storageBucket: "*************",
+  messagingSenderId: "**************",
+  appId: "**************************"
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+
 export const db = getFirestore(app);
